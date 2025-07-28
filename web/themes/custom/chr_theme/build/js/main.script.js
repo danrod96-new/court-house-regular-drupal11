e8990a3cc9a503243d2b09cf38813231f882b026
Daniel Rodriguez <danrod@gmail.com>
2025-07-28 01:20:12 -0400

    More styling / Tests with Radix

--- web/themes/custom/chr_theme/build/js/main.script.js
+++ web/themes/custom/chr_theme/build/js/main.script.js
@@ -1,6 +1,71 @@
 /******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./components/html/html.scss":
+/*!***********************************!*\
+  !*** ./components/html/html.scss ***!
+  \***********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./components/nav/nav.scss":
+/*!*********************************!*\
+  !*** ./components/nav/nav.scss ***!
+  \*********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./components/page-content/page-content.scss":
+/*!***************************************************!*\
+  !*** ./components/page-content/page-content.scss ***!
+  \***************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./components/page-footer/page-footer.scss":
+/*!*************************************************!*\
+  !*** ./components/page-footer/page-footer.scss ***!
+  \*************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./components/page/page.scss":
+/*!***********************************!*\
+  !*** ./components/page/page.scss ***!
+  \***********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./node_modules/@popperjs/core/lib/createPopper.js":
 /*!*********************************************************!*\
   !*** ./node_modules/@popperjs/core/lib/createPopper.js ***!
@@ -8394,7 +8459,12 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/build/js/main.script": 0,
-/******/ 			"build/css/main.style": 0
+/******/ 			"components/page-footer/page-footer": 0,
+/******/ 			"components/page-content/page-content": 0,
+/******/ 			"components/nav/nav": 0,
+/******/ 			"components/html/html": 0,
+/******/ 			"build/css/main.style": 0,
+/******/ 			"components/page/page": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -8446,8 +8516,13 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["build/css/main.style"], function() { return __webpack_require__("./src/js/main.script.js"); })
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["build/css/main.style"], function() { return __webpack_require__("./src/scss/main.style.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["components/page-footer/page-footer","components/page-content/page-content","components/nav/nav","components/html/html","build/css/main.style","components/page/page"], function() { return __webpack_require__("./src/js/main.script.js"); })
+/******/ 	__webpack_require__.O(undefined, ["components/page-footer/page-footer","components/page-content/page-content","components/nav/nav","components/html/html","build/css/main.style","components/page/page"], function() { return __webpack_require__("./src/scss/main.style.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["components/page-footer/page-footer","components/page-content/page-content","components/nav/nav","components/html/html","build/css/main.style","components/page/page"], function() { return __webpack_require__("./components/html/html.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["components/page-footer/page-footer","components/page-content/page-content","components/nav/nav","components/html/html","build/css/main.style","components/page/page"], function() { return __webpack_require__("./components/nav/nav.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["components/page-footer/page-footer","components/page-content/page-content","components/nav/nav","components/html/html","build/css/main.style","components/page/page"], function() { return __webpack_require__("./components/page-content/page-content.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["components/page-footer/page-footer","components/page-content/page-content","components/nav/nav","components/html/html","build/css/main.style","components/page/page"], function() { return __webpack_require__("./components/page-footer/page-footer.scss"); })
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["components/page-footer/page-footer","components/page-content/page-content","components/nav/nav","components/html/html","build/css/main.style","components/page/page"], function() { return __webpack_require__("./components/page/page.scss"); })
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
