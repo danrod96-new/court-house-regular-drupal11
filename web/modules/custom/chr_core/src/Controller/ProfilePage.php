21db106cb21c29b6c0400eb48b0ef0f60c0a0644
Daniel Rodriguez <danrod@gmail.com>
2025-08-09 06:09:18 -0400

    Added more pages to the profile section

--- web/modules/custom/chr_core/src/Controller/ProfilePage.php
+++ web/modules/custom/chr_core/src/Controller/ProfilePage.php
@@ -17,6 +17,67 @@ class ProfilePage extends ControllerBase {
    */
   public function affiliatePage() {
     return [
+      '#theme' => 'affiliate_center',
+      '#user' => \Drupal::currentUser(),
+      '#current_path' => \Drupal::request()->getPathInfo(),
+      '#title_page' => "Affiliate Center",
+      '#sales_today' => 0,
+      '#sales_last_seven_days' => 0,
+      '#sales_last_year' => 0,
+      '#unique_todays_clicks' => 0,
+      '#unique_last_seven_days' => 0,
+      '#unique_last_year' => 0, 
+    ];
+  }
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function bookmarksPage() {
+    return [
+      '#theme' => 'bookmarks_page',
+      '#user' => \Drupal::currentUser(),
+      '#current_path' => \Drupal::request()->getPathInfo(),
+      '#title_page' => "Bookmarks",
+      '#bookmarks' => [],
+    ];
+  }
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function friendsPage() {
+    return [
+      '#markup' => $this->t('Hello, world from my custom controller!'),
+    ];
+  }
+
+  /**
+   * This might be a form.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function invitePage() {
+    return [
+      '#markup' => $this->t('Hello, world from my custom controller!'),
+    ];
+  }
+
+  /**
+   * This might be also a form.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function inviteByMailPage() {
+    return [
       '#markup' => $this->t('Hello, world from my custom controller!'),
     ];
   }
