0c3e12bc8a37cdbc96e7a2bc1f4833adeae539b5
Daniel Rodriguez <danrod@gmail.com>
2026-05-23 15:46:55 -0400

    Added more missing functionality

--- web/modules/custom/chr_core/src/Form/ChrConfigFormInvite.php
+++ web/modules/custom/chr_core/src/Form/ChrConfigFormInvite.php
@@ -6,16 +6,16 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Config settings form for the CHR site.
+ * Invite settings form for the CHR site.
  */
-class ChrConfigForm extends ConfigFormBase {
+class ChrConfigFormInvite extends ConfigFormBase {
 
   /**
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
     return [
-      'chr_core.adminsettings',
+      'chr_core.adminsettingsinvite',
     ];
   }
 
@@ -23,21 +23,27 @@ class ChrConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'chr_core_settings_form';
+    return 'chr_core_settings_form_invite';
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('chr_core.adminsettings');
+    $config = $this->config('chr_core.adminsettingsinvite');
+
+    $subject = "[yourname] has sent you an invite!";
+
+    $body = "[yourname] has invited you to join Court House Regular at [home_link].
+    To become a member of Court House Regular, click the link below or 
+    paste it into the address bar of your browser.[registration_link]";
 
     $form['title'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Subject'),
       '#size' => 128,
       '#maxlength' => 128,
-      '#default_value' => $config->get('invite_subject') ?? "",
+      '#default_value' => $config->get('invite_subject') ?? $subject,
       '#description' => $this->t("Enter default e-mail subject."),
       '#required' => TRUE,
     ];
@@ -46,7 +52,7 @@ class ChrConfigForm extends ConfigFormBase {
       '#type' => 'textarea',
       '#title' => $this->t('Message Body'),
       '#description' => $this->t('Enter default e-mail body.'),
-      '#default_value' => $config->get('message_body') ?? "",
+      '#default_value' => $config->get('message_body') ?? $body,
     ];
 
     return parent::buildForm($form, $form_state);
@@ -58,11 +64,11 @@ class ChrConfigForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    $this->config('chr_core.adminsettings')
+    $this->config('chr_core.adminsettingsinvite')
       ->set('invite_subject', $form_state->getValue('title'))
       ->save();
 
-    $this->config('chr_core.adminsettings')
+    $this->config('chr_core.adminsettingsinvite')
       ->set('message_body', $form_state->getValue('message_body'))
       ->save();
   }
